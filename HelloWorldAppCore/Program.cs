07c1d876d08c3573f9baa58b1b8f0ffd40700c0d
MRebai13K <moez.rebaii@gmail.com>
2018-05-13 20:27:08 +0200

    Add new user enhancement

--- HelloWorldAppCore/Program.cs
+++ HelloWorldAppCore/Program.cs
@@ -6,7 +6,16 @@ namespace HelloWorldAppCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Users :");
+
+            Console.WriteLine("Please enter your first name :");
+            var firstname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name :");
+            var lastname = Console.ReadLine();
+
+            var user = new User(firstname, lastname);
+            Console.WriteLine(user.ToString());
             Console.ReadKey();
         }
     }
