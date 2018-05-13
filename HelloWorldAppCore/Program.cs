bcc14224183229282c2a03428f4fa11c83bddfbb
MRebai13K <moez.rebaii@gmail.com>
2018-05-13 20:06:46 +0200

    Add project files.

---
+++ HelloWorldAppCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
